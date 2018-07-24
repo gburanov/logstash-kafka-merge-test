f4211c5579b7d12cf2acc27f39ed67934a2a9483
Georgy Buranov <gburanov@gmail.com>
2018-07-24 18:00:13 +0200

    Create several pipelines

--- image/kafka/create_topics.sh
+++ image/kafka/create_topics.sh
@@ -3,5 +3,7 @@
 set -x
 
 /opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users1
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users2
+
+# Not relly needed - done by logstash
+# /opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users1
+# /opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic users2
